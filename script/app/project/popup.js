a57d118db83cfd4b2bed06c0812442ac2eadfc6f
Gavin.Li <liwirong@gmail.com>
2013-05-16 17:33:31 +0800

    Added MP event

---
+++ script/app/project/popup.js
@@ -0,0 +1,20 @@
+/**
+ * User: Gavin.Li
+ * Date: 5/16/13
+ * Module: popup
+ */
+;define(function(require, exports, module){
+    var $ = require("jquery"),
+        _ = require("underscore"),
+        ev = PM.event,
+        Backbone = require("backbone");
+    require("jquery.ui");
+    require("jsrender");
+
+
+    return{
+        open:function(){
+
+        }
+    }
+});
