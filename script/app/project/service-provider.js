673879beace5486a7db3c334e94e07bf6bbf3a22
oracis <robin.zhou@dextrys.com>
2013-05-16 17:37:06 +0800

    Added data

--- script/app/project/service-provider.js
+++ script/app/project/service-provider.js
@@ -3,30 +3,31 @@
  * Date: 5/16/13
  */
 ;define(function(require, exports, module){
-        var $ = require("jquery"),
-            _ = require("underscore"),
-            Backbone = require("backbone"),
-            data = require('./data');
+    var $ = require("jquery"),
+        _ = require("underscore"),
+        Backbone = require("backbone"),
+        data = require("./data");
 
-    var ItemModel = Backbone.Model.extend({
-
-    });
+    var ItemModel = Backbone.Model.extend({});
 
     var ItemList = Backbone.Collection.extend({
-        model:ItemModel
+        model : ItemModel
     });
 
     var ItemView = Backbone.View.extend({
-        tagName:"tr",
-        template:_.template($('#provider-tpl').html()),
-        render:function () {
-            this.$el.html(this.template(this.model.toJSON()));
-            return this;
+        tagName: "tr",
+        className: "",
+        template :_.template($('#provider-tpl').html()),
+        events: {
+            "click .tt":"operate"
         },
-        initialize:function () {
+        initialize: function () {
             this.render();
         },
-        events:{
+        render : function () {
+            var model = this.model;
+            model.set("status",data.status[1*(model.get("status"))]);
+            this.$el.html(this.template(this.model.toJSON()));
         },
         operate:function () {   // button operate
 
@@ -34,34 +35,31 @@
     });
 
     var AppView = Backbone.View.extend({
-        initialize:function (renderTarget) {
+        initialize : function(renderTarget){
             this.renderTarget = renderTarget;
-            //this.setElement($('#specsTableContainer'));
             this.collection = new ItemList();
             this.collection.bind("add", this.renderOne, this);
             this.collection.bind("reset", this.renderAll, this);
             this.getData();
         },
-        events:{
-
-        },
-        renderOne:function (item, index) {
+        renderOne : function(item){
             var itemView = new ItemView({model:item});
-            $(this.renderTarget).append(itemView.el);
+            this.renderTarget.append(itemView.el);
         },
-        renderAll:function () {
-            $(this.renderTarget).empty();
-            this.collection.each($.proxy(this.renderOne, this));
+        renderAll : function(){
+            this.renderTarget.empty();
+            this.collection.each($.proxy(this.renderOne,this));
         },
-        getData:function () {
-            debugger;
-            this.collection.reset(data);
+        getData : function () {
+            var arr = _.filter(data.data, function(item){ return item.status !== 0;})
+            this.collection.reset(arr);
         }
+
     });
 
     return {
         init:function () {
-            new AppView("#specsTableContainerTbody");
+            new AppView($("#specsTableContainerTbody"));
         }
     }
 
