f407201b5200d7df93666a54ffbab14ad7cdc68d
Gavin.Li <liwirong@gmail.com>
2013-05-16 19:39:01 +0800

    Added MP event

--- script/app/project/data.js
+++ script/app/project/data.js
@@ -14,6 +14,7 @@
                 'status':0,
                 'completionDate':'03/31/2013',
                 'CreationDate':'01/5/2013',
+                'desc':'Photo Print desc',
                 'recent':'10:00am'
             },
             {
@@ -23,6 +24,7 @@
                 'status':1,
                 'completionDate':'05/20/2013',
                 'CreationDate':'02/13/2013',
+                'desc':'Book Sell desc',
                 'recent':'three days ago'
             },
             {
@@ -32,6 +34,7 @@
                 'status':1,
                 'completionDate':'02/19/2013',
                 'CreationDate':'09/16/2012',
+                'desc':'English training desc',
                 'recent':'Yesterday'
             },
             {
@@ -41,6 +44,7 @@
                 'status':2,
                 'completionDate':'04/15/2013',
                 'CreationDate':'10/20/2012',
+                'desc':'Primary school education desc',
                 'recent':'11:00am'
             },
             {
@@ -50,6 +54,7 @@
                 'status':1,
                 'completionDate':'05/26/2013',
                 'CreationDate':'07/21/2012',
+                'desc':'Spring Poster Compaign desc',
                 'recent':'12:00am'
             }
         ]
--- script/app/project/popup.js
+++ script/app/project/popup.js
@@ -6,29 +6,111 @@
 ;define(function(require, exports, module){
     var $ = require("jquery"),
         _ = require("underscore"),
-        ev = window.MP.event,
-        Backbone = require("backbone");
+        MP = window.MP,
+        ev = MP.event,
+        data = MP.data.data,
+        Backbone = require("backbone"),
+        undefined,
+        id = 6,
+        dataModel = {
+            'id':0,
+            'name':'',
+            'owner':'',
+            'status':0,
+            'completionDate':'',
+            'CreationDate':'',
+            'desc':'',
+            'recent':''
+        };
     require("jquery.ui");
     require("jsrender");
     var itemView = Backbone.View.extend({
         tagName:"div",
-        events:{
-
-        },
         template:_.template($("#popup-tpl").html()),
         render:function(){
             this.$el.html(this.template(this.model.toJSON()));
-            this.$el.dialog({
-                resizable: false,
-                height:140,
-                modal: true,
-                buttons: {
-                    "Delete all items": function() {
-                        $( this ).dialog( "close" );
-                    },
-                    Cancel: function() {
+            var
+                buttonTextArr = "Create,Save,Submit".split(","),
+                ownerTypeArr = "Buyer,Service".split(","),
+                optTypeArr = "Add,Update".split(","),
+                model = this.model.toJSON(),
+                btn1txt = buttonTextArr[model.type + model.ownerType],
+                actionKey =[ownerTypeArr[model.ownerType],optTypeArr[model.type]].join(".");
+
+            var buttons = [
+                {
+                    text: btn1txt,
+                    click: function() {
+                        var
+                            nameIpt = $( this).find("[name='name']"),
+                            completionDateIpt = $( this).find("[name='completionDate']"),
+                            descIpt = $( this).find("[name='desc']"),
+                            ownerIpt = $( this).find("[name='ownerIpt']");
+                        if(!nameIpt.val()) return
+                        var addData = _.extend({},dataModel,model,{
+                            name:nameIpt.val(),
+                            owner:ownerIpt.val(),
+                            status:[0,1,2][Math.ceil(Math.random()*10%2)],
+                            completionDate:completionDateIpt.val(),
+                            desc:descIpt.val()
+                        });
+                        model.type == 0&&(addData.id=id++);
+
+                        data.push(addData);
+                        var cp =ownerTypeArr.slice(0);
+                        cp.push("");
+                        //make 'Buyer.Create Service.Create'
+                        ev.trigger(cp.join("."+buttonTextArr[0]+" "),addData);
+                        console.log(cp.join("."+buttonTextArr[0]+" "));
                         $( this ).dialog( "close" );
                     }
+                },
+                {
+                    text: 'cancel',
+                    click: function() {
+
+                        $( this ).dialog( "close" );
+                    }
+                }
+            ];
+
+            if(model.ownerType===0){
+                buttons.splice(1,0,{
+                    text: 'Save as Draft',
+                    click: function() {
+                        var
+                            nameIpt = $( this).find("[name='name']"),
+                            completionDateIpt = $( this).find("[name='completionDate']"),
+                            descIpt = $( this).find("[name='desc']"),
+                            ownerIpt = $( this).find("[name='owner']");
+                        if(!nameIpt.val()) return
+                        var addData = _.extend({},dataModel,model,{
+                            name:nameIpt.val(),
+                            owner:ownerIpt.val(),
+                            status:[0,1,2][Math.ceil(Math.random()*10%2)],
+                            completionDate:completionDateIpt.val(),
+                            desc:descIpt.val()
+                        });
+                        model.type == 0&&(addData.id=id++);
+
+                        data.push(addData);
+                        ev.trigger(actionKey,addData);
+                        console.log(actionKey);
+                        $( this ).dialog( "close" );
+                    }
+                })
+            }
+            this.$el.dialog({
+                resizable: false,
+                height:300,
+                width:600,
+                modal: true,
+                create:function(){
+
+                },
+                buttons: buttons,
+                close:function(){
+                    $(this).remove();
                 }
             });
         },
@@ -43,10 +125,12 @@
          * open
          * @param data
          * @param type 0:copy 1:editor
-         * @param owner 0:buyer 1:service
+         * @param ownerType 0:buyer 1:service
          */
-        open:function(data,type,owner){
+        open:function(data,type,ownerType){
             data = data||{};
+            data.type = type == undefined?0:type;
+            data.ownerType = ownerType == undefined?0:ownerType;
             new itemView({model:new Backbone.Model(data)});
         }
     }
