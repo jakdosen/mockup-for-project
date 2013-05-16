17a2741a7fb2a2cdd35c30c62caaca65cfe53713
Gavin.Li <liwirong@gmail.com>
2013-05-16 15:22:41 +0800

    Added data

--- script/app/project/data.js
+++ script/app/project/data.js
@@ -5,6 +5,53 @@
  */
 ;define(function(require, exports, module){
     return{
-
+        status:['Draft','Estimates Received','Order Received'],
+        data:[
+            {
+                'id':1,
+                'name':'Photo Print',
+                'owner':'Gavin',
+                'status':0,
+                'completionDate':'03/31/2013',
+                'CreationDate':'01/5/2013',
+                'recent':'10:00am'
+            },
+            {
+                'id':2,
+                'name':'Book Sell',
+                'owner':'Bob',
+                'status':1,
+                'completionDate':'05/20/2013',
+                'CreationDate':'02/13/2013',
+                'recent':'three days ago'
+            },
+            {
+                'id':3,
+                'name':'English training',
+                'owner':'Robin',
+                'status':1,
+                'completionDate':'02/19/2013',
+                'CreationDate':'09/16/2012',
+                'recent':'Yesterday'
+            },
+            {
+                'id':4,
+                'name':'Primary school education',
+                'owner':'Song',
+                'status':2,
+                'completionDate':'04/15/2013',
+                'CreationDate':'10/20/2012',
+                'recent':'11:00am'
+            },
+            {
+                'id':5,
+                'name':'Spring Poster Compaign',
+                'owner':'Yang Luan',
+                'status':1,
+                'completionDate':'05/26/2013',
+                'CreationDate':'07/21/2012',
+                'recent':'12:00am'
+            }
+        ]
     }
 });
